1e83022a959f7526e7148c7fb648e6f00382a6d5
Pavel Grechikhin <pav.gre4ixin@yandex.ru>
2022-06-06 23:57:48 +0400

    Appear

--- DangerTest/DangerTest/ContentView.swift
+++ DangerTest/DangerTest/ContentView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
             .padding()
+            .onAppear {
+                text = "TEST APPEAR"
+            }
     }
 }
+
+//Comment
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
